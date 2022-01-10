30ab58431630fcf7afb6856debd206b84e71f7c2
Dat0309 <anhtheboy011@gmail.com>
2022-01-10 20:05:09 +0700

    get started

--- get_started_flutter/lib/main.dart
+++ get_started_flutter/lib/main.dart
@@ -1,7 +1,63 @@
 import 'package:flutter/material.dart';
 
-import 'MyApp.dart';
-
 void main() {
-  runApp(MyApp("trong dat", 21));
+  runApp(MyAppp());
+}
+
+class MyAppp extends StatelessWidget {
+  const MyAppp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.red[300]),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Get started"),
+          leading: IconButton(
+            icon: Icon(Icons.ac_unit),
+            onPressed: () {
+              print('leading');
+            },
+          ),
+        ),
+        body: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                child: Text('Line 1'),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Text('Line 2'),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Text('Line 3'),
+              )
+            ],
+          ),
+        ));
+  }
 }
