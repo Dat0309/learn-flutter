b4205e253acc6244d52992405c6c42a29cd058ae
Dat0309 <anhtheboy011@gmail.com>
2022-03-14 12:34:44 +0700

    update

--- homme_service_app/lib/models/service.dart
+++ homme_service_app/lib/models/service.dart
@@ -6,36 +6,13 @@ class Service {
 }
 
 List<Service> services = [
-  Service(
-      name: 'Cleaning',
-      imgUrl:
-          'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-cleaning-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
-  Service(
-      name: 'Plumber',
-      imgUrl:
-          'https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/2x/external-plumber-labour-day-vitaliy-gorbachev-flat-vitaly-gorbachev.png'),
-  Service(
-      name: 'Electrician',
-      imgUrl:
-          'https://img.icons8.com/external-wanicon-flat-wanicon/2x/external-multimeter-car-service-wanicon-flat-wanicon.png'),
-  Service(
-      name: 'Painter',
-      imgUrl:
-          'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-painter-male-occupation-avatar-itim2101-flat-itim2101.png'),
-  Service(
-      name: 'Carpenter', imgUrl: 'https://img.icons8.com/fluency/2x/drill.png'),
-  Service(
-      name: 'Gardener',
-      imgUrl:
-          'https://img.icons8.com/external-itim2101-flat-itim2101/2x/external-gardener-male-occupation-avatar-itim2101-flat-itim2101.png'),
-  Service(
-      name: 'Tailor',
-      imgUrl: 'https://img.icons8.com/fluency/2x/sewing-machine.png'),
-  Service(
-      name: 'Maid',
-      imgUrl: 'https://img.icons8.com/color/2x/housekeeper-female.png'),
-  Service(
-      name: 'Driver',
-      imgUrl:
-          'https://img.icons8.com/external-sbts2018-lineal-color-sbts2018/2x/external-driver-women-profession-sbts2018-lineal-color-sbts2018.png'),
+  Service(name: 'Cleaning', imgUrl: 'assets/images/cleaning.png'),
+  Service(name: 'Plumber', imgUrl: 'assets/images/pluber.png'),
+  Service(name: 'Electrician', imgUrl: 'assets/images/eletrician.png'),
+  Service(name: 'Painter', imgUrl: 'assets/images/painter.png'),
+  Service(name: 'Carpenter', imgUrl: 'assets/images/carpenter.png'),
+  Service(name: 'Gardener', imgUrl: 'assets/images/gardener.png'),
+  Service(name: 'Tailor', imgUrl: 'assets/images/tailor.png'),
+  Service(name: 'Maid', imgUrl: 'assets/images/maid.png'),
+  Service(name: 'Driver', imgUrl: 'assets/images/driver.png'),
 ];
--- homme_service_app/lib/pages/start_page.dart
+++ homme_service_app/lib/pages/start_page.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:homme_service_app/animation/fadeanimation.dart';
 import 'package:homme_service_app/models/service.dart';
@@ -55,6 +53,84 @@ class _StartPageState extends State<StartPage> {
               },
             ),
           ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(80),
+                  topRight: Radius.circular(80),
+                ),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FadeAnimation(
+                    delay: 1.5,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Center(
+                        child: Text(
+                          'Easy, reliable way take \ncare of yoour home',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade900,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                    delay: 1.5,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 60),
+                      child: Center(
+                        child: Text(
+                          'We provide yoou with the best people to help take of your home.',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  FadeAnimation(
+                    delay: 1.5,
+                    child: Padding(
+                      padding: EdgeInsets.all(50.0),
+                      child: MaterialButton(
+                        elevation: 0,
+                        color: Colors.black,
+                        onPressed: () {},
+                        height: 55,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -70,8 +146,8 @@ class _StartPageState extends State<StartPage> {
           color: selectedService == index ? Colors.white : Colors.grey.shade100,
           border: Border.all(
             color: selectedService == index
-                ? Colors.blue.shade100
-                : Colors.grey.withOpacity(0),
+                ? Color.fromARGB(255, 94, 95, 95)
+                : Color.fromARGB(255, 44, 44, 44).withOpacity(0),
             width: 2.0,
           ),
           borderRadius: BorderRadius.circular(15.0),
@@ -79,7 +155,7 @@ class _StartPageState extends State<StartPage> {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.network(
+              Image.asset(
                 image,
                 height: 30,
                 fit: BoxFit.cover,
